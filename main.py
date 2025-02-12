ca7de7fbdf00f6020482289a08266f39125e5d9c
Swooshoo <eliotlee1415@gmail.com>
2025-02-11 18:01:41 -0800

    #1: Implemented a Quick filter DAG chart to search up pokemon. Need to implement input from the chart to generating radar charts

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 import dash
+import dash_ag_grid as dag
+from dash import Dash, html, dcc, Input, Output, callback, Patch
+import pandas as pd
 from dash import dcc, html
 from dash.dependencies import Input, Output
 from src.pokemon import pokemon_info, generate_radar
 
+from dash import Patch
+import pandas as pd
+
 app = dash.Dash(__name__)
+
+#example dataset column defined
+# example_df = pd.read_csv(
+#     "https://raw.githubusercontent.com/plotly/datasets/master/ag-grid/olympic-winners.csv"
+# )
+
+# columnDefs = [
+#     {
+#         "field": "athlete",
+#         "checkboxSelection": True,
+#         "headerCheckboxSelection": True,
+#         "headerCheckboxSelectionFilteredOnly": True,
+#     },
+#     {"field": "age"},
+#     {"field": "country"},
+#     {"field": "year"},
+#     {"field": "sport"},
+#     {"field": "total"},
+# ]
+
+new_df = pd.read_csv("151pokemon.csv")
+
+newColDefs = [
+    {"field": "pokedex"},
+    {
+        "field": "pokemon",
+        "checkboxSelection": True,
+        "headerCheckboxSelection": True,
+        "headerCheckboxSelectionFilteredOnly": True,
+    },
+    {"field": "type1"},
+    {"field": "type2"},
+]
 
 # Example function that can be called from main.py
 def get_pokemon_data(pokedex_number):
@@ -15,7 +54,6 @@ def get_pokemon_data(pokedex_number):
 # Dash app layout
 app.layout = html.Div([
     html.H1("Pokémon Type Effectiveness"),
-    
     # Input field for user to enter Pokédex number
     dcc.Input(id="pokemon-input", type="number", placeholder="Enter Pokédex number"),
     
@@ -23,8 +61,19 @@ app.layout = html.Div([
     html.Div(id="pokemon-info"),
     
     # Display radar chart
-    dcc.Graph(id="radar-chart")
-])
+    dcc.Graph(id="radar-chart"),
+    
+    dcc.Input(id="input-radio-row-selection-checkbox-header-filtered-only", placeholder="Quick filter..."),
+        dag.AgGrid(
+            id="row-selection-checkbox-header-filtered-only",
+            columnDefs=newColDefs,
+            rowData=new_df.to_dict("records"),
+            columnSize="sizeToFit",
+            defaultColDef={"filter": True},
+            dashGridOptions={"rowSelection": "multiple", "animateRows": False},
+        ),
+    ],
+)
 
 # Callback to update Pokémon info and radar chart based on input
 @app.callback(
@@ -36,6 +85,15 @@ def display_pokemon_info(pokedex_number):
         info_output, radar_figure = get_pokemon_data(int(pokedex_number))
         return html.Div([html.Pre(info_output)]), radar_figure
     return "Enter a valid Pokémon Pokédex number.", {}
+
+@app.callback(
+    Output("row-selection-checkbox-header-filtered-only", "dashGridOptions"),
+    Input("input-radio-row-selection-checkbox-header-filtered-only", "value"),
+)
+def update_filter(filter_value):
+    gridOptions_patch = Patch()
+    gridOptions_patch["quickFilterText"] = filter_value
+    return gridOptions_patch
 
 if __name__ == "__main__":
     app.run_server(debug=True)
